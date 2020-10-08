ece857275b7816f4e58446e5aede1d2fef6cd8ef
elberth moraes <elberth.moraes@gmail.com>
2020-10-08 11:24:32 -0300

    Recebendo parâmetros via args

--- Primeiro.java
+++ Primeiro.java
@@ -1,5 +1,12 @@
 public class Primeiro{
 	public static void main(String[] args){
-		System.out.println("Elberth Moraes");
+		
+		//caractere = String (textos)
+		//inteiro = int (inteiros)
+		//real = float (casas decimais)
+		//boleano = boolean (true/false)
+		
+		System.out.println("Nome: " + args[0]);
+		System.out.println("Sobrenome: " + args[1]);
 	}
 }
